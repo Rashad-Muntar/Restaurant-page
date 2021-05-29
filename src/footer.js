b4b045afdec9c4c209f5143bb88c5d13f0541346
Rashad-Muntar <muntartoure@gmail.com>
2021-05-29 16:35:35 +0000

    add footer

---
+++ src/footer.js
@@ -0,0 +1,15 @@
+function footerContent() {
+  const footer = document.createElement('footer');
+  const wrapper = document.createElement('div');
+  const text = document.createElement('p');
+
+  wrapper.classList.add('wrapper');
+  text.innerHTML = 'Desig and built by Rashad';
+
+  wrapper.appendChild(text);
+  footer.appendChild(wrapper);
+
+  return footer;
+}
+
+export default footerContent;
--- src/page.js
+++ src/page.js
@@ -1,10 +1,12 @@
 /* eslint-disable import/no-cycle */
 import header from './header';
 import { homeContent } from './home';
+import footerContent from './footer';
 
 function page() {
   const content = document.getElementById('content');
   content.appendChild(header());
   content.appendChild(homeContent());
+  document.body.appendChild(footerContent());
 }
 export default page;
